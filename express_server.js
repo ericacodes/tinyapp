e5fd17efcbe71ab9097cb8dd5fabc879864ac12b
Erica Sun <ericasun@seas.upenn.edu>
2020-06-09 18:11:16 +0000

    add client-side form to urls_index template and server-side router to delete URLs

--- express_server.js
+++ express_server.js
@@ -44,6 +44,11 @@ app.post("/urls", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls");
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
