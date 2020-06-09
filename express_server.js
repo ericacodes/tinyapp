0e7ab2475a412a8f469f4a9ff37add90013f95f9
Erica Sun <ericasun@seas.upenn.edu>
2020-06-09 17:17:42 +0000

    implement POST /urls route as (generate short URL, add short and long URLs to urlDatabase, redirect to GET /urls)

--- express_server.js
+++ express_server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const PORT = 8080; // default port 8080
+const PORT = 8080;
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
@@ -11,44 +11,37 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
- });
- 
- app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
- });
-
  app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
-app.get("/urls/new", (req, res) => {
+// more specific 
+app.get("/urls/new", (req, res) => { 
   res.render("urls_new");
 });
 
+// less specific 
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  let longURL = urlDatabase[req.params.shortURL];
+  if (!urlDatabase[req.params.shortURL].startsWith('http')) {
+    longURL = `http://${urlDatabase[req.params.shortURL]}`;
+  }; 
+  res.redirect(longURL);
+});
+
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  const short = generateRandomString();
+  urlDatabase[short] = req.body.longURL;
+  res.redirect(`/urls/${short}`);
+  let templateVars = { shortURL : req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  res.render("urls_show", templateVars);
 });
 
 app.listen(PORT, () => {
@@ -63,3 +56,8 @@ function generateRandomString() {
   }
   return result;
 }
+
+
+
+
+
