62837e435c7935a6bd91a5607904deccb1cec276
Erica Sun <ericasun@seas.upenn.edu>
2020-06-09 22:19:01 +0000

    add client-side form and server-side router to edit URLs

--- express_server.js
+++ express_server.js
@@ -49,6 +49,13 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   res.redirect("/urls");
 });
 
+app.post("/urls/:shortURL", (req, res) => {
+  console.log(req.params.longURL);
+  console.log(typeof req.params.longURL);
+  urlDatabase[req.params.shortURL] = req.body.longURL;
+  res.redirect("/urls");
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
